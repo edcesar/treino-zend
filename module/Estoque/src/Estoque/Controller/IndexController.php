34b296a4d4794c434d66a01b49903a4685d978df
Ed Cesar <edcesar@edcesar.com>
2017-02-01 15:22:07 +0000

    criado array $produtos ficticio e passado para view. Listando na tabela ok

--- module/Estoque/src/Estoque/Controller/IndexController.php
+++ module/Estoque/src/Estoque/Controller/IndexController.php
@@ -8,10 +8,14 @@ class IndexController extends AbstractActionController
 {
 	public function indexAction()
 	{
-		$msg = 'Bem vindo ao Zend Framework!'; 
+		$produtos = [];
+
+		$produtos[] = ['nome' =>'Playstation 4', 'preco' => '2700', 'descricao' => 'Video Game da Sony'];
+		$produtos[] = ['nome' =>'Xbox 360', 'preco' => '2500', 'descricao' => 'Video game da Microsoft'];
+		$produtos[] = ['nome' =>'Nitendo Wii', 'preco' => '1500', 'descricao' => 'Video game da Nitendo'];
 
 
-		$viewParans = array('msg' => $msg);
+		$viewParans = array('produtos' => $produtos);
 
 
 		return new ViewModel($viewParans);
